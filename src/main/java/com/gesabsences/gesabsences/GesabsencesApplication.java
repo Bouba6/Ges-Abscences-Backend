afe920cb47be34bfa4dbadcfea6f65c9d30e6466
booba6 <seydinaaboubacasadikhbathily@gmail.com>
2025-05-26 11:29:11 -0700

    first commit

---
+++ src/main/java/com/gesabsences/gesabsences/GesabsencesApplication.java
@@ -0,0 +1,13 @@
+package com.gesabsences.gesabsences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesabsencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesabsencesApplication.class, args);
+	}
+
+}
